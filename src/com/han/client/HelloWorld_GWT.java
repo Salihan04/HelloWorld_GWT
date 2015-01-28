4bffe890cab2aac689306e74280aad32f5fac0f4
unknown <t_bins@EISSGH107PYFN.ads.autodesk.com>
2015-01-28 13:56:24 +0800

    Added buttons to implement CSS styling

--- src/com/han/client/HelloWorld_GWT.java
+++ src/com/han/client/HelloWorld_GWT.java
@@ -1,7 +1,9 @@
 package com.han.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.RootPanel;
 
 /**
@@ -10,8 +12,30 @@ import com.google.gwt.user.client.ui.RootPanel;
 public class HelloWorld_GWT implements EntryPoint {
 	
 	public void onModuleLoad() {
-		HTML html = new HTML("<p>Welcome to GWT application</p>");
 		
-		RootPanel.get("gwtContainer").add(html);
+		//Add button to change font to big when clicked
+		Button btn1 = new Button("Big Text");
+		btn1.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				// TODO Auto-generated method stub
+				RootPanel.get("mytext").setStyleName("gwt-Big-Text");
+			}
+		});
+		
+		//Add button to change font to small when clicked
+		Button btn2 = new Button("Small Text");
+		btn2.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				// TODO Auto-generated method stub
+				RootPanel.get("mytext").setStyleName("gwt-Small-Text");
+			}
+		});
+		
+		RootPanel.get("gwtGreenButton").add(btn1);
+		RootPanel.get("gwtRedButton").add(btn2);
 	}
 }
