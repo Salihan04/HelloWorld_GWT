09487817d99fb6974e60107463fb21f2ffdc9f29
unknown <t_bins@EISSGH107PYFN.ads.autodesk.com>
2015-01-28 11:53:10 +0800

    Created simple Hello World application using GWT

---
+++ src/com/han/client/HelloWorld_GWT.java
@@ -0,0 +1,14 @@
+package com.han.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class HelloWorld_GWT implements EntryPoint {
+	
+	public void onModuleLoad() {
+		Window.alert("Hello World!");
+	}
+}
