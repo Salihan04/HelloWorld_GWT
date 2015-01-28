f851856f54d18c4d8be40d695e5943e719a4ac36
unknown <t_bins@EISSGH107PYFN.ads.autodesk.com>
2015-01-28 12:17:44 +0800

    Prepared GWT application for deployment on apache tomcat server

--- src/com/han/client/HelloWorld_GWT.java
+++ src/com/han/client/HelloWorld_GWT.java
@@ -1,7 +1,8 @@
 package com.han.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.RootPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -9,6 +10,8 @@ import com.google.gwt.user.client.Window;
 public class HelloWorld_GWT implements EntryPoint {
 	
 	public void onModuleLoad() {
-		Window.alert("Hello World!");
+		HTML html = new HTML("<p>Welcome to GWT application</p>");
+		
+		RootPanel.get("gwtContainer").add(html);
 	}
 }
