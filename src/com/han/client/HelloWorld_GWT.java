bd18aa26a32b3812ff8a3dfe49453c428cc7c8cc
unknown <t_bins@EISSGH107PYFN.ads.autodesk.com>
2015-01-29 12:03:12 +0800

    Implemented History class with GWT

--- src/com/han/client/HelloWorld_GWT.java
+++ src/com/han/client/HelloWorld_GWT.java
@@ -1,89 +1,85 @@
 package com.han.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DecoratorPanel;
-import com.google.gwt.user.client.ui.HasHorizontalAlignment;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.event.logical.shared.SelectionEvent;
+import com.google.gwt.event.logical.shared.SelectionHandler;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.TabPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class HelloWorld_GWT implements EntryPoint {
-	
-	private MessageServiceAsync messageService = GWT.create(MessageService.class);
-	
-	private class MessageCallBack implements AsyncCallback<Message> {
-
-		@Override
-		public void onFailure(Throwable caught) {
-			//Server side error occured
-			Window.alert("Unable to obtain server response: " + caught.getMessage());
-			
-		}
-
-		@Override
-		public void onSuccess(Message result) {
-			//Server returned result, show user message
-			Window.alert(result.getMessage());
-			
-		}
-	}
 
 	public void onModuleLoad() {
-		//Create UI
-		final TextBox txtName = new TextBox();
-		txtName.setWidth("200");
-		txtName.addKeyUpHandler(new KeyUpHandler() {
+		//Create a tab panel to carry multiple pages
+		final TabPanel tabPanel = new TabPanel();
+		
+		//Create pages
+		HTML firstPage = new HTML("<h1>We are on the first page.</h1>");
+		HTML secondPage = new HTML("<h1>We are on the second page.</h1>");
+		HTML thirdPage = new HTML("<h1>We are on the third page.</h1>");
+		
+		String firstPageTitle = "First Page";
+		String secondPageTitle = "Second Page";
+		String thirdPageTitle = "Third Page";
+		tabPanel.setWidth("400");
+		
+		//Add pages to tabPanel
+		tabPanel.add(firstPage, firstPageTitle);
+		tabPanel.add(secondPage, secondPageTitle);
+		tabPanel.add(thirdPage, thirdPageTitle);
+		
+		//Add tab selection handler
+		tabPanel.addSelectionHandler(new SelectionHandler<Integer>() {
 			
 			@Override
-			public void onKeyUp(KeyUpEvent event) {
-				if(event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					//Make remote call to server to get the message
-					messageService.getMessage(txtName.getValue(), new MessageCallBack());
+			public void onSelection(SelectionEvent<Integer> event) {
+				/*Add a token to history containing pageIndex
+				 * History class will change the URL of the application
+				 * by appending the token to it
+				 */
+				History.newItem("pageIndex" + event.getSelectedItem());
+			}
+		});
+		
+		/*Add value change handler to History
+		 * this method will be called when browser's
+		 * Back button or Forward button are clicked 
+		 * and URL of application changes
+		 */
+		History.addValueChangeHandler(new ValueChangeHandler<String>() {
+			
+			@Override
+			public void onValueChange(ValueChangeEvent<String> event) {
+				String historyToken = event.getValue();
+				//Parse the history token
+				try {
+					if(historyToken.substring(0, 9).equals("pageIndex")) {
+						String tabIndexToken = historyToken.substring(9, 10);
+						int tabIndex = Integer.parseInt(tabIndexToken);
+						
+						//Select the selected tab panel
+						tabPanel.selectTab(tabIndex);
+					}
+					else {
+						tabPanel.selectTab(0);
+					}
+				}
+				catch(IndexOutOfBoundsException e) {
+					tabPanel.selectTab(0);
 				}
 			}
 		});
 		
-		Label lblName = new Label("Enter your name: ");
-		
-		Button buttonMessage = new Button("Click Me!");
-		buttonMessage.addClickHandler(new ClickHandler() {
-			
-			@Override
-			public void onClick(ClickEvent event) {
-				//Make remote server call to get the message
-				messageService.getMessage(txtName.getValue(), new MessageCallBack());
-			}
-		});
-		
-		HorizontalPanel hPanel = new HorizontalPanel();
-		hPanel.add(lblName);
-		hPanel.add(txtName);
-		hPanel.setCellWidth(lblName, "130");
-		
-		VerticalPanel vPanel = new VerticalPanel();
-		vPanel.setSpacing(10);
-		vPanel.add(hPanel);
-		vPanel.add(buttonMessage);
-		vPanel.setCellHorizontalAlignment(buttonMessage, HasHorizontalAlignment.ALIGN_RIGHT);
-		
-		DecoratorPanel dPanel = new DecoratorPanel();
-		dPanel.add(vPanel);
+		//Select the first tab by default
+		tabPanel.selectTab(0);
 		
 		//Add widgets to root panel
-		RootPanel.get("gwtContainer").add(dPanel);
+		RootPanel.get("gwtContainer").add(tabPanel);
 	}
 }
